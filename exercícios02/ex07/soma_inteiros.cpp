b24b7b65a239261d4563c8fc83ae357134b0a81a
Nicolas Duque <92602576+Nicolasduquee@users.noreply.github.com>
2023-03-11 23:45:29 -0300

    exercicio em c

---
+++ exercícios02/ex07/soma_inteiros.cpp
@@ -0,0 +1,30 @@
+/*Criar um programa em linguagem C que
+leia dois números inteiros e somá-los.
+Se a soma for maior que 10, mostrar o
+resultado. Senão exibir a mensagem
+que não é possível exibir o número.*/
+
+#include <windows.h>
+#include <stdio.h>
+
+main(){
+	system("cls");
+	
+	printf("Soma Inteiros\n\n");
+	
+	int v1, v2, s;
+
+	printf("Digite dois valores intiros:\n");
+	scanf("%d%d", &v1, &v2);
+	
+	s = v1 + v2;
+	
+	if (s > 10){
+		printf("Resultado da soma: %d\n", s);
+	}
+	else{
+		printf("Nao e possivel exibir o numero\n");
+	}
+	
+	system("pause");
+}
